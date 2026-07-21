2d6dee6fd3e445abe2d15fd5517638ed0d2ea232
Moussou Fadillou Koulibaly <40604369+MoussouFadillou@users.noreply.github.com>
2026-07-21 16:04:34 +0000

    Create database.py

---
+++ database.py
@@ -0,0 +1,47 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL est absente")
+
+
+# Railway peut fournir une URL postgres://
+# SQLAlchemy attend postgresql://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+Base = declarative_base()
+
+
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
