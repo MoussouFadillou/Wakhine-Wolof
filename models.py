ce6a99600bde305fc15fd0459392bf932a1ca018
Moussou Fadillou Koulibaly <40604369+MoussouFadillou@users.noreply.github.com>
2026-08-10 19:37:43 +0000

    Update models.py

--- models.py
+++ models.py
@@ -13,61 +13,73 @@ class Contribution(Base):
 
     __tablename__ = "contributions"
 
+
     id = Column(
         Integer,
         primary_key=True,
         index=True
     )
 
+
     age = Column(
         Integer,
         nullable=False
     )
+
 
     sexe = Column(
         String(50),
         nullable=False
     )
 
+
     region = Column(
         String(100),
         nullable=False
     )
+
 
     departement = Column(
         String(100),
         nullable=False
     )
 
+
     accent = Column(
         String(150),
         nullable=False
     )
+
 
     alphabetisation = Column(
         String(200),
         nullable=False
     )
 
+
     type_parole = Column(
         String(200),
         nullable=False
     )
+
 
     transcription = Column(
         Text,
         nullable=True
     )
 
+
     audio_url = Column(
         Text,
         nullable=False
     )
 
+
     google_drive_file_id = Column(
         String(255),
         nullable=False
     )
+
 
     created_at = Column(
         DateTime,
